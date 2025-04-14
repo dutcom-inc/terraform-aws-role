095c0e73f34de58d62afff295f7d1595c4cf9bf6
damiend <damien.dutech@stack-labs.com>
2025-04-14 15:06:17 +0200

    edit - policies path

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "default" {
     name = local.name
-    assume_role_policy = "${file("policies/${var.assume_role_policy_name}")}"
+    assume_role_policy = "${file("${path.module}/policies/${var.assume_role_policy_name}")}"
 
   tags = merge(
     local.common_tags
